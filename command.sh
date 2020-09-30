7f6f4264a0fb4baec57b8fe3f11cd36d18f461cd
jhljx <jhljx8918@gmail.com>
2020-09-30 16:17:44 +0800

    add bash to run GKT variants

---
+++ command.sh
@@ -0,0 +1,5 @@
+python train.py --data-file=daily_2019_kp_all.csv --graph-type=VAE;
+python train.py --data-file=daily_2019_kp_all.csv --graph-type=MHA;
+python train.py --data-file=daily_2019_kp_all.csv --graph-type=Dense;
+python train.py --data-file=daily_2019_kp_all.csv --graph-type=Transition;
+python train.py --data-file=daily_2019_kp_all.csv --graph-type=PAM;
